158a1d6d9c68cd139d4a0072b45ae6cfef4db066
Max <shmx.dev@gmail.com>
2021-01-27 14:59:49 +0200

    env setup

---
+++ App/App/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Максим Шаптала on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
